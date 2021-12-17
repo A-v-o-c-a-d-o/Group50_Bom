4faba6527b8cabc1ec31c2f9b722803cfa23a904
Tuan Anh <mravocado@gmail.com>
2021-12-17 11:02:08 +0700

    edit oneal

--- Code/Entity/Moveable/Enemys/Oneal.java
+++ Code/Entity/Moveable/Enemys/Oneal.java
@@ -17,11 +17,11 @@ public class Oneal extends Enemy {
     public Oneal(int x, int y, int step) {
         super(x, y, step);
         dieSound = "scarabIdle.wav";
-        imgLeft = new Image("./Resources/icons/oneal_left1.png");
-        imgRight = new Image("./Resources/icons/oneal_right1.png");
-        imgUp = new Image("./Resources/icons/oneal_left2.png");
-        imgDown = new Image("./Resources/icons/oneal_right2.png");
-        imgDead = new Image("./Resources/icons/oneal_dead.png");
+        imgLeft = new Image("./Resources/icons/oneal_left.png");
+        imgRight = new Image("./Resources/icons/oneal_right.png");
+        imgUp = new Image("./Resources/icons/oneal.png");
+        imgDown = new Image("./Resources/icons/oneal.png");
+        imgDead = new Image("./Resources/icons/oneal.png");
         image = imgDown;
     }
 
